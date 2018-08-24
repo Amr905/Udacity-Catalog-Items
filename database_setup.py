b3f228488bff483a33f5058543c35901c0ad3189
Amr Morsy <amr905@gmail.com>
2018-08-24 22:30:39 +0200

    uploading flask app

---
+++ database_setup.py
@@ -0,0 +1,35 @@
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+
+
+class Category(Base):
+    __tablename__ = 'category'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+
+class Item(Base):
+    __tablename__ = 'item'
+    name = Column(String(100), nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String(250))
+    category_id = Column(Integer, ForeignKey('category.id'))
+    categories = relationship(Category)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+
+engine = create_engine('sqlite:///catalogues.db')
+Base.metadata.create_all(engine)
